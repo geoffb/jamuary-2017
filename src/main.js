bcb0a55063a8e2dc714f02520c3d717dbe322843
Geoff Blair <gmblair@gmail.com>
2017-01-07 12:57:40 -0800

    Text rendering and transform fixes

--- src/main.js
+++ src/main.js
@@ -5,17 +5,32 @@ document.body.appendChild(surface);
 var context = surface.getContext("2d");
 
 var Node = require("scene/Node");
+var Text = require("scene/Text");
 
 var box = new Node();
 box.x = 10;
 box.y = 10;
 box.resize(100, 100);
 box.color = "cyan";
-box.scaleX = 2;
-box.scaleY = 2;
-box.rotation = Math.PI / 4;
+// box.scaleX = 2;
+// box.scaleY = 2;
+// box.rotation = Math.PI / 4;
 
-context.fillStyle = "cornflowerblue";
-context.fillRect(0, 0, surface.width, surface.height);
+var title = new Text("foo", 50);
+title.x = 100;
+title.y = 100;
+title.color = "#00FF00";
+title.scaleX = 2;
+title.rotation = Math.PI / 4;
 
-box.render(context);
+var render = function () {
+  context.fillStyle = "cornflowerblue";
+  context.fillRect(0, 0, surface.width, surface.height);
+
+  box.render(context);
+  title.render(context);
+
+  requestAnimationFrame(render);
+};
+
+render();
