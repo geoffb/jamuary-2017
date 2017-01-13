92bf767d52a735eb0bb1ac64e381f784f17e32f1
Geoff Blair <gmblair@gmail.com>
2017-01-13 15:40:23 -0800

    TileMap and other stuff

--- src/main.js
+++ src/main.js
@@ -7,11 +7,19 @@ var context = surface.getContext("2d");
 var Node = require("scene/Node");
 var Text = require("scene/Text");
 var Sprite = require("scene/Sprite");
-var atlas = require("scene/atlas");
+var assets = require("scene/assets");
+var TileMap = require("scene/TileMap");
+var Map = require("math/Map");
 
-var atlasData = require("./atlas.js");
+var atlas = require("./atlas.js");
 
-atlas.load("images/atlas.png", atlasData);
+assets.loadTextureAtlas("atlas", "images/atlas.png", atlas);
+
+var map = new Map(5, 5, 32, 32);
+
+for (let i = 0; i < map.width; ++i) {
+  map.set(i, i, 1);
+}
 
 var box = new Node();
 box.x = 10;
@@ -21,7 +29,7 @@ box.color = "cyan";
 box.scaleY = 2;
 box.rotation = Math.PI / 4;
 
-var title = new Text("foo", 50);
+var title = new Text("foobarbaz", 50);
 title.x = 100;
 title.y = 100;
 title.color = "#00FF00";
@@ -34,6 +42,10 @@ foo.y = 100;
 foo.rotation = Math.PI / 4;
 foo.scale = 2;
 
+var tileMap = new TileMap();
+tileMap.map = map;
+tileMap.textureKey = "atlas";
+
 var render = function () {
   context.fillStyle = "cornflowerblue";
   context.fillRect(0, 0, surface.width, surface.height);
@@ -41,6 +53,7 @@ var render = function () {
   box.render(context);
   title.render(context);
   foo.render(context);
+  tileMap.render(context);
 
   requestAnimationFrame(render);
 };
