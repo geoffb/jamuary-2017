1ff0da4fb3f35458dc1671a5c4aedca8701a1ed1
Geoff Blair <gmblair@gmail.com>
2018-01-06 17:12:19 -0800

    Mortal.damage triggers brief immune state

--- src/sim/components/Mortal.js
+++ src/sim/components/Mortal.js
@@ -11,8 +11,20 @@ let Mortal = module.exports = function (data) {
 object.extend(Mortal, Emitter);
 
 Mortal.prototype.damage = function (damage) {
+  let states = this.entity.getComponent("states");
+
+  // Ignore damage if entity is immune
+  if (states.has("immune")) { return; }
+
+  // Reduce health
   this.health -= damage;
-  this.emit("healthChange", this.health);
+
+  // Grant a brief immunity
+  states.add("immune", 400);
+
+  this.emit("healthChange", this.health, -damage);
+
+  // Check for death
   if (this.health <= 0) {
     this.entity.removeFromLevel();
   }
